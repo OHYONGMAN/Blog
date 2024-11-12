5c349dec611d83fe02677a7279f4a7db0b41cf4f
OHYONGMAN <oymin1998@naver.com>
2024-11-12 16:29:41 +0900

    docs: 필요없는 파일 제거

--- store/hero-store.jsx
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useHeroStore = create((set) => ({
-  heroImage: "/images/photo.jpg", // 기본 Hero 이미지
-  setHeroImage: (imageUrl) => set({ heroImage: imageUrl }),
-}));
-
-export default useHeroStore;
